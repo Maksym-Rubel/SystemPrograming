b6d53a33d7c39b3c79f080a402814d934e8350fd
Maks <rubelmaksum2404@gmail.com>
2025-03-24 19:22:01 +0200

    Add project files.

---
+++ 03_GetArguments/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        foreach(var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+        Console.ReadKey();
+
+    }
+}
