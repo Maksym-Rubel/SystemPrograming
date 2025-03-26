72f9fee9dc43615611dc06e754d0760591cb9d64
Maks <rubelmaksum2404@gmail.com>
2025-03-26 20:44:42 +0200

    nerw

---
+++ 05_thread_example_app/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _05_thread_example_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread t1 = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            t1 = new Thread(HardWork);
+            t1.Start();
+            
+        }
+        private void HardWork()
+        {
+            bool flag = false;
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                if (progress.Value > 0)
+                    progress.Value = progress.Minimum;
+                flag = progress.Value < progress.Maximum;
+            });
+           
+
+            while(flag)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    progress.Value++;
+                flag = progress.Value < progress.Maximum;
+                });
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ 06_Class_thread/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Net.Sockets;
+using System.Threading;
+
+internal class Program
+{
+    private static int max = 0;
+    private static int min = 100;
+    private static double average = 0;
+    private static int[] numbers;
+    private static void Main(string[] args)
+    {
+        int for_count = 10_000;
+        numbers = new int[for_count];
+        int num = 0;
+        Random random = new Random();
+        for (int i = 0; i < for_count; i++)
+        {
+            numbers[i] = random.Next(1, 101);
+
+            Console.WriteLine(numbers[i]);
+            
+
+
+        }
+        Thread Tmax = new Thread(Max);
+        
+        Thread Tmin = new Thread(Min);
+   
+        Thread Taverage = new Thread(Average);
+   
+        Thread Tfiles = new Thread(File);
+        Tmax.Start();
+        Tmin.Start();
+        Taverage.Start();
+    
+        Tmax.Join();
+        Tmin.Join();
+        Taverage.Join();
+
+        Tfiles.Start();
+        Tfiles.Join();
+        Console.WriteLine($"Max --> {max}");
+        Console.WriteLine($"Max --> {min}");
+        Console.WriteLine($"Average --> {average}");
+
+        
+    }
+
+    static void Max()
+    {
+        
+
+        foreach (var num in numbers)
+        {
+            
+                if (num > max)
+                {
+                    max = num;
+                }
+            
+        }
+    }
+    static void Min()
+    {
+
+        foreach (var num in numbers)
+        {
+
+            if (num < min)
+            {
+                min = num;
+            }
+
+        }
+    }
+    static void Average()
+    {
+
+        average = numbers.Average();
+
+    }
+    static void File()
+    {
+        
+        string path = @"numbers.txt";
+        using (StreamWriter sw = new StreamWriter(path,true))
+        {
+            sw.WriteLine("Згенеровані числа:");
+            sw.WriteLine(string.Join("\n", numbers));
+            sw.WriteLine($"Максимальне число: {max}");
+            sw.WriteLine($"Мінімальне число: {min}");
+            sw.WriteLine($"Середнє арифметичне: {average}");
+        }
+        
+    }
+}
