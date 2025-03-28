99bf7152bcc50bf8e0582a842896df2fd35938da
Maks <rubelmaksum2404@gmail.com>
2025-03-28 20:06:07 +0200

    nwq

---
+++ 07_classmonitor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    class LetterCounter()
+    {
+        int wordCount;
+        int riadCount;
+        int rozdiloviCount;
+        public int WordCount { get { return wordCount; } }
+        public int RiadCount { get { return riadCount; } }
+        public int RozdiloviCount { get { return rozdiloviCount; }  }
+
+        public void UpdateFields(object a)
+        {
+            string path = a.ToString();
+            using (StreamReader sw = new StreamReader(path))
+            {
+                lock (this)
+                {
+                    riadCount = sw.ReadToEnd().Split('\n').Count();
+                    string alltext = File.ReadAllText(path);
+                    char[] rozdilovi = { '.', ',', ';', ':', '-', '—', '…', '!', '?', '"', '«', '»',
+                     '(', ')', '{', '}', '[', ']', '<', '>', '/'};
+                    rozdiloviCount = alltext.Count(c => rozdilovi.Contains(c));
+                    wordCount = alltext.Split(new char[] { ' ', '\t', '\n' }).Count();
+                }
+                
+
+            }
+
+        }
+    }
+
+
+    private static void Main(string[] args)
+    {
+        LetterCounter c = new LetterCounter();
+        string path = "new.txt";
+        Thread threads = new Thread(c.UpdateFields);
+        threads.Start(path);
+        threads.Join();
+        Console.WriteLine(c.RiadCount);
+        Console.WriteLine(c.RozdiloviCount);
+        Console.WriteLine(c.WordCount);
+
+
+
+    
+    }
+}
