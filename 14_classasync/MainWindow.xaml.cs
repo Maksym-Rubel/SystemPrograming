c60a36370e88d7e218058eefa7e172e323aefc2b
Maks <rubelmaksum2404@gmail.com>
2025-04-09 20:47:33 +0300

    new

--- 14_classasync/MainWindow.xaml.cs
+++ 14_classasync/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Win32;
+﻿using IOExtensions;
+using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Dialogs;
+using PropertyChanged;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
@@ -20,22 +24,30 @@ namespace _14_classasync
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string Source { get; set; }
-        public string Target { get; set; }
+        ViewModel model;
+        int id = 0;
         public MainWindow()
         {
+
             InitializeComponent();
-            srcTextBox.Text = Source = @"D:\Test\1GB.bin";
-            destTextBox.Text = Target = @"C:\Users\Maksym\Desktop\TestCopy";
+            model = new ViewModel()
+            {
+                Source = @"D:\Test\1GB.bin",
+                Destination = @"C:\Users\Maksym\Desktop\TestCopy",
+                Progress = 0,
+            };
+            srcTextBox.Text = model.Source;
+            destTextBox.Text = model.Destination;
+            this.DataContext = model;
 
         }
 
         private void OpenFileBtn(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog();
-            if(dialog.ShowDialog() == true)
+            if (dialog.ShowDialog() == true)
             {
-                srcTextBox.Text = Source = dialog.FileName;
+                srcTextBox.Text = model.Source = dialog.FileName;
             }
         }
 
@@ -43,48 +55,99 @@ namespace _14_classasync
         {
             CommonOpenFileDialog dialog = new CommonOpenFileDialog();
             dialog.IsFolderPicker = true;
-            if(dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
-                destTextBox.Text = Target = dialog.FileName;
+                destTextBox.Text = model.Destination = dialog.FileName;
             }
         }
 
         private async void CopyFile(object sender, RoutedEventArgs e)
         {
-
-            string fileName = System.IO.Path.Combine(Target, System.IO.Path.GetFileName(srcTextBox.Text));
+            string fileName = System.IO.Path.GetFileName(srcTextBox.Text);
+            string destfileName = System.IO.Path.Combine(model.Destination,$"{fileName}({id++})");
             //File.Copy(Source, Targert1);
-
-            await CopyFileAsync(Source, fileName);
-           
+            CopyProcessInfo info = new CopyProcessInfo(fileName);
+            model.AddProcess(info);
+          
+            await CopyFileAsync(model.Source, destfileName,info);
+            info.Percentage = 100;
             MessageBox.Show("Complate");
         }
-        private Task CopyFileAsync(string source, string dest)
+        private Task CopyFileAsync(string source, string dest, CopyProcessInfo info)
         {
-            return Task.Run(() =>
+            return FileTransferManager.CopyWithProgressAsync(source, dest,(progress) =>
             {
-                using (FileStream scrFile = new FileStream(source, FileMode.Open, FileAccess.Read))
-                {
-                    using (FileStream destFile = new FileStream(dest, FileMode.Create, FileAccess.Write))
-                    {
-                        byte[] buffer = new byte[1024 * 8];
-                        int bytes = 0;
-                        do
-                        {
-                            bytes = scrFile.Read(buffer, 0, buffer.Length);
-                            destFile.Write(buffer, 0, bytes);
-                            float percentage = destFile.Length / (scrFile.Length / 100);
-                            Dispatcher.Invoke(() => { 
-                                progress.Value = percentage;
-                                percentage_.Text = $"{percentage}%";
-                            });
-                        
-                           
+                model.Progress = progress.Percentage;
+                info.Percentage = progress.Percentage;
+                info.BytesPerSeconds = progress.BytesPerSecond;
 
-                        } while (bytes > 0);
-                    }
-                }
-            });
+            },false);
+            //return Task.Run(() =>
+            //{
+            //    using (FileStream scrFile = new FileStream(source, FileMode.Open, FileAccess.Read))
+            //    {
+            //        using (FileStream destFile = new FileStream(dest, FileMode.Create, FileAccess.Write))
+            //        {
+            //            byte[] buffer = new byte[1024 * 8];
+            //            int bytes = 0;
+            //            do
+            //            {
+            //                bytes = scrFile.Read(buffer, 0, buffer.Length);
+            //                destFile.Write(buffer, 0, bytes);
+            //                float percentage = destFile.Length / (scrFile.Length / 100);
+            //                model.Progress = percentage;
+
+
+            //            } while (bytes > 0);
+            //        }
+            //    }
+            //});
+
+
+        }
+
+
+
+
+
+    }
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        private ObservableCollection<CopyProcessInfo> processes;
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public double Progress { get; set; }
+        public bool IsWaiting => Progress == 0;
+
+        public ViewModel()
+        {
+            processes = new ObservableCollection<CopyProcessInfo>();
+        }
+        public IEnumerable<CopyProcessInfo> Processes => processes;
+
+
+        public void AddProcess(CopyProcessInfo info)
+        {
+            processes.Add(info);
+        }
+
+    }
+    [AddINotifyPropertyChangedInterface]
+    public class CopyProcessInfo
+    {
+        
+        public string FileName { get; set; }
+        public double Percentage { get; set; }
+
+        public int PercentageInt => (int)Percentage;
+        public double BytesPerSeconds;
+        public double MegaBytesPerSeconds => Math.Round(BytesPerSeconds / 1024 / 1024,1);
+
+
+        public CopyProcessInfo(string fileName)
+        {
+            FileName = fileName;
         }
     }
 }
---
+++ 16_class/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Task 1
+        List<int> numbers = new List<int> { };
+        using (StreamReader sr = new StreamReader("new.txt"))
+        {
+            string[] number = sr.ReadToEnd().Split(new[] { ' ', ',', '\n', '\r' });
+            foreach (var num in number)
+            {
+                int num1 = int.Parse(num);
+                numbers.Add(num1);
+            }
+        }
+        Console.Write("Numbers --> ");
+        foreach (var num in numbers)
+        {
+            Console.Write(num + "\t");
+        }
+        Console.WriteLine();
+        int uniqueNumbersCount = numbers.AsParallel()
+                           .Where(n => IsUnicua(n, numbers))
+                           .ToList()
+                           .Count();
+
+        Console.WriteLine("Unique numbers --> " + uniqueNumbersCount);
+
+        //Task 2
+        List<int> numbers1 = new List<int> { };
+        using (StreamReader sr1 = new StreamReader("new2.txt"))
+        {
+            string[] number = sr1.ReadToEnd().Split(new[] { ' ', ',', '\n', '\r' });
+            foreach (var num in number)
+            {
+                int num1 = int.Parse(num);
+                numbers1.Add(num1);
+            }
+        }
+        Console.Write("Numbers --> ");
+        foreach (var num in numbers1)
+        {
+            Console.Write(num + "\t");
+        }
+        Console.WriteLine();
+
+        int UpperCount = IsUpper(numbers1); // незнаю як тут з PLINQ викликати
+
+
+
+        Console.WriteLine("Upper numbers --> " + UpperCount);
+    }
+
+    public static bool IsUnicua(int num, List<int> numbers)
+    {
+        int count = 0;
+        foreach (var item in numbers)
+        {
+            if (item == num)
+            {
+                count++;
+                if(count > 1)
+                {
+                    return false;
+                }
+                
+            }
+        }
+        return true;
+
+
+
+    }
+    public static int IsUpper(List<int> numbers)
+    {
+        int maxCount = 0;
+        int upperCount = 1;
+        for (int i = 1; i < numbers.Count; i++)
+        {
+            if (numbers[i] > numbers[i - 1])
+            {
+                upperCount++;
+            }
+            else
+            {
+                if(upperCount > maxCount)
+                {
+                    maxCount = upperCount;
+                }
+                upperCount = 1;
+            }
+        }
+
+        return maxCount;
+
+
+
+    }
+}
